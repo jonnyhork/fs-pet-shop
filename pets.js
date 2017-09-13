da055cefb05e6aa0f4b8e027c6d6db6fa9e9534b
Jonny Hork <JonnyHork@gmail.com>
2017-09-13 11:44:04 -0600

    fs-pets complete

--- pets.js
+++ pets.js
@@ -1,139 +1,142 @@
- 'use strict';
+#! /usr/bin/env node
 
- let fs = require('fs')
- let path = require('path')
- let petsPath = path.join(__dirname, 'pets.json')
+'use strict';
 
- let node = path.basename(process.argv[0])
- let file = path.basename(process.argv[1])
- let cmd = process.argv[2]
+let fs = require('fs')
+let path = require('path')
+let petsPath = path.join(__dirname, 'pets.json')
 
-
- if (cmd === 'read') {
-
-   fs.readFile(petsPath, 'utf8', (err, data) => {
-     let index = process.argv[3]
-     let pets = JSON.parse(data)
-
-     if (err) {
-       throw err
-     } else if (index > pets.length - 1) {
-       console.error(`Usage: ${node} ${file} read INDEX`)
-       process.exit(1)
-     } else if (index === 0 || index) {
-       console.log(pets[index])
-     } else {
-       console.log(pets)
-     }
-   })
-
- } else if (cmd === 'create') {
-   fs.readFile(petsPath, 'utf8', (readErr, data) => {
-
-     let pets = JSON.parse(data)
-     let age = process.argv[3]
-     let kind = process.argv[4]
-     let name = process.argv[5]
-
-     let newPet = {
-       age: Number(`${age}`),
-       kind: `${kind}`,
-       name: `${name}`
-     }
-
-     if (readErr) {
-       throw readErr
-     }
-
-     if (!age || !kind || !name) {
-       console.error(`Usage: ${node} ${file} create AGE KIND NAME`)
-       process.exit(1)
-     } else {
-       pets.push(newPet)
-     }
+let node = path.basename(process.argv[0])
+let file = path.basename(process.argv[1])
+let cmd = process.argv[2]
 
 
+if (cmd === 'read') {
 
-     let petsJSON = JSON.stringify(pets)
+  fs.readFile(petsPath, 'utf8', (err, data) => {
+    let index = process.argv[3]
+    let pets = JSON.parse(data)
 
-     fs.writeFile(petsPath, petsJSON, (writeErr) => {
-       if (writeErr) {
-         throw writeErr
-       }
-       console.log(newPet);
-     })
-   })
- } else if (cmd === 'update') {
+    if (err) {
+      throw err
+    } else if (index > pets.length - 1) {
+      console.error(`Usage: ${node} ${file} read INDEX`)
+      process.exit(1)
+    } else if (index === 0 || index) {
+      console.log(pets[index])
+    } else {
+      console.log(pets)
+    }
+  })
 
-   fs.readFile(petsPath, 'utf8', (updateErr, data) => {
+} else if (cmd === 'create') {
+  fs.readFile(petsPath, 'utf8', (readErr, data) => {
 
-     if (updateErr) {
-       console.error(updateErr);
-     }
+    let pets = JSON.parse(data)
+    let age = process.argv[3]
+    let kind = process.argv[4]
+    let name = process.argv[5]
 
-     const pets = JSON.parse(data)
-     const idx = process.argv[3]
-     const age = process.argv[4]
-     const kind = process.argv[5]
-     const name = process.argv[6]
+    let newPet = {
+      age: Number(`${age}`),
+      kind: `${kind}`,
+      name: `${name}`
+    }
 
-     let updatePet = {
+    if (readErr) {
+      throw readErr
+    }
 
-       age: Number(`${age}`),
-       kind: `${kind}`,
-       name: `${name}`
-
-     }
-
-     if (!idx || !age || !kind || !name) {
-       console.error(`Usage: ${node} ${file} update INDEX AGE KIND NAME`)
-       process.exit(1)
-     } else {
-       pets[idx] = updatePet
-     }
+    if (!age || !kind || !name) {
+      console.error(`Usage: ${node} ${file} create AGE KIND NAME`)
+      process.exit(1)
+    } else {
+      pets.push(newPet)
+    }
 
 
 
-     const petsJSON = JSON.stringify(pets)
+    let petsJSON = JSON.stringify(pets)
 
-     fs.writeFile(petsPath, petsJSON, (writeErr) => {
-       if (writeErr) {
-         console.error("update write error: ", writeErr);
-       }
-       console.log(updatePet);
-     })
+    fs.writeFile(petsPath, petsJSON, (writeErr) => {
+      if (writeErr) {
+        throw writeErr
+      }
+      console.log(newPet);
+    })
+  })
+} else if (cmd === 'update') {
 
-   })
+  fs.readFile(petsPath, 'utf8', (updateErr, data) => {
 
- } else if (cmd === 'destroy') {
+    if (updateErr) {
+      console.error(updateErr);
+    }
 
-   fs.readFile(petsPath, 'utf8', (destroyErr, data) => {
+    const pets = JSON.parse(data)
+    const idx = process.argv[3]
+    const age = process.argv[4]
+    const kind = process.argv[5]
+    const name = process.argv[6]
 
-     if (destroyErr) {
-       console.error("destroy error is: ", destroyErr);
-     }
-     const pets = JSON.parse(data)
-     const idx = process.argv[3]
+    let updatePet = {
 
-     if (!idx) {
-       console.error(`Usage: ${node} ${file} destroy INDEX`);
-       process.exit(1)
-     } else {
-       pets.splice(Number(idx), 1)
-     }
+      age: Number(`${age}`),
+      kind: `${kind}`,
+      name: `${name}`
+
+    }
+
+    if (!idx || !age || !kind || !name) {
+      console.error(`Usage: ${node} ${file} update INDEX AGE KIND NAME`)
+      process.exit(1)
+    } else {
+      pets[idx] = updatePet
+    }
 
 
-     const petsJSON = JSON.stringify(pets)
 
-     fs.writeFile(petsPath, petsJSON, (writeErr) => {
-       if (writeErr) {
-         console.error("destroy write error", writeErr);
-       }
+    const petsJSON = JSON.stringify(pets)
 
-     })
-   })
- } else {
+    fs.writeFile(petsPath, petsJSON, (writeErr) => {
+      if (writeErr) {
+        console.error("update write error: ", writeErr);
+      }
+      console.log(updatePet);
+    })
 
-   console.error(`Usage: ${node} ${file} [read | create | update | destroy]`)
-   process.exit(1)
- }
+  })
+
+} else if (cmd === 'destroy') {
+
+  fs.readFile(petsPath, 'utf8', (destroyErr, data) => {
+
+    if (destroyErr) {
+      console.error("destroy error is: ", destroyErr);
+    }
+    const pets = JSON.parse(data)
+    const idx = process.argv[3]
+
+    if (!idx) {
+      console.error(`Usage: ${node} ${file} destroy INDEX`);
+      process.exit(1)
+    } else {
+      console.log(pets[idx]);
+      pets.splice(Number(idx), 1)
+    }
+
+
+    const petsJSON = JSON.stringify(pets)
+
+    fs.writeFile(petsPath, petsJSON, (writeErr) => {
+      if (writeErr) {
+        console.error("destroy write error", writeErr);
+      }
+
+    })
+  })
+} else {
+
+  console.error(`Usage: ${node} ${file} [read | create | update | destroy]`)
+  process.exit(1)
+}
