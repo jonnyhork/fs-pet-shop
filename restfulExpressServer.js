cdb7044c870b991a44ed7b186925f24872f1aea7
Jonny Hork <JonnyHork@gmail.com>
2017-09-15 08:48:00 -0600

    working on PATCH, getting a 404 wild card error

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -15,8 +15,9 @@ const router = express.Router()
 
 
 router.get('/pets', (_req, res) => {
-  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
-    if (err) {
+  fs.readFile(petsPath, 'utf8', (readErr, petsJSON) => {
+    if (readErr) {
+      console.error('GET read error:', readErr)
       return res.sendStatus(500)
     }
 
@@ -27,8 +28,9 @@ router.get('/pets', (_req, res) => {
 })
 
 router.get('/pets/:idx', (req, res) => {
-  fs.readFile(petsPath, 'utf8', (err, petsJSON) => {
-    if (err) {
+  fs.readFile(petsPath, 'utf8', (readErr, petsJSON) => {
+    if (readErr) {
+      console.error('GET read error:', readErr)
       return res.sendStatus(500)
     }
 
@@ -51,14 +53,18 @@ router.post('/pets', (req, res) => {
 
   fs.readFile(petsPath, 'utf8', (readErr, petsJSON) => {
     if (readErr) {
-      console.error(readErr)
+      console.error('POST read error:', readErr)
       return res.sendStatus(500)
     }
 
     let petsData = JSON.parse(petsJSON)
     let age = Number(req.body.age)
-    let kind = req.body.kind
-    let name = req.body.name
+    // let kind = req.body.kind
+    // let name = req.body.name
+    let {
+      kind,
+      name
+    } = req.body //ES6 syntax for assigning multiple variables to key value pairs. only obj?
 
     if (isNaN(age) || !kind || !name) {
       return res.sendStatus(400)
@@ -75,14 +81,64 @@ router.post('/pets', (req, res) => {
 
     fs.writeFile(petsPath, petsJSON, (writeErr) => {
       if (writeErr) {
-        console.error(writeErr);
+        console.error('POST write error:', writeErr);
         return res.sendStatus(500)
       }
       res.send(newPet)
     })
   })
 
+  router.patch('/pets/:idx', (req, res) => {
+    fs.readFile(petsPath, 'utf8', (readErr, petsJSON) => {
 
+      if (readErr) {
+        console.error('PATCH read error:', readErr)
+        return res.sendStatus(500)
+      }
+
+      let idx = Number(req.params.idx)
+      let petsData = JSON.parse(petsJSON)
+
+      if (idx < 0 || idx >= petsData.length || Number.isNaN(idx)) {
+        console.log('patch error')
+        return res.sendStatus(404)
+      }
+
+      let patchPet = petsData[idx]
+      let age = Number(req.body.age)
+      let {
+        kind,
+        name
+      } = req.body
+
+      // if (isNaN(age) || !kind || !name) {
+      //   return res.sendStatus(400)
+      // }
+
+      if (age) {
+        patchPet.age = age
+      }
+
+      if (kind) {
+        patchPet.kind = kind
+      }
+
+      if (name) {
+        patchPet.name = name
+      }
+
+      petsJSON = JSON.stringify(petsData)
+
+      fs.writeFile(petsPath, petsJSON, (writeErr) => {
+        if (writeErr) {
+          console.error('PATCH write error:', writeErr);
+          return res.sendStatus(500)
+        }
+        res.send(patchPet)
+      })
+
+    })
+  })
 
 
 
